0f81adc0bf2d9627e93402e8927412c08f3cb622
Akuro1411 <nahidisaye147@gmail.com>
2024-05-18 10:29:25 +0000

    Changes lowercase letters to upper

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        number = ord(i)
+        if number >= 97 and number <= 122:
+            number = number - 32
+            print(chr(number), end="")
+        else:
+            print(i, end="")
