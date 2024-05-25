2c530970a6e0d94e883b7fa9659bb2bf5dd12fe9
Akuro1411 <nahidisayev147@gmail.com>
2024-05-25 21:17:11 +0000

    Adds elements of 2 tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = list(tuple_a)
+    b = list(tuple_b)
+    length_a = len(a)
+    length_b = len(b)
 
+    if length_a > length_b:
+        t = length_a - length_b
+        for i in range(t):
+            b.append(0)
+    elif length_b > length_a:
+        t = length_b - length_a
+        for i in range(t):
+            a.append(0)
+
+    new_list = []
+    for j in range(len(a)):
+        new_list.append(a[j] + b[j])
+    new_tup = tuple(new_list)
+    return new_tup
