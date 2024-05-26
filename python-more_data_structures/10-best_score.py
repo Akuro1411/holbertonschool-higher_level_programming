e9cb95137d0639416e54af38a42253bb2a8cd9af
Akuro1411 <nahidisayev147@gmail.com>
2024-05-26 05:45:21 +0000

    Nothing was added instead of None

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary == None:
-        return None
+        return 
     num = 0
     for i in a_dictionary:
         if num == 0 and a_dictionary[i] != 0:
