d65a2b3bf6f17c5a8f8552869c6b2e56597f0354
Akuro1411 <nahidisayev147@gmail.com>
2024-05-26 05:14:21 +0000

    Finds the maximum score of the class

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    num = 0
+    for i in a_dictionary:
+        if num == 0 and a_dictionary[i] != 0:
+            num = a_dictionary[i]
+        if a_dictionary[i] > num:
+            num = a_dictionary[i]
+    return num
