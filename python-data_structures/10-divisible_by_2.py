bffe03573755a231b0dc0c9a736903c5e14647e0
Akuro1411 <nahidisayev147@gmail.com>
2024-05-26 19:36:54 +0000

    The error is handled

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    new_array = []
-    for i in my_list:
-        if i % 2 == 0:
-            new_array.append(True)
-        else:
-            new_array.append(False)
-    return new_array
+def best_score(a_dictionary):
+    """Returns a key with the biggest integer value."""
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
