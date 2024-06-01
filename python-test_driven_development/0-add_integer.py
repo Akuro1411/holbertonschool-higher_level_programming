0899b44dd62ce450e916fc7bc59430c9548e2b52
Akuro1411 <nahidisayev147@gmail.com>
2024-06-01 16:45:45 +0000

    module and function are documentes

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+No module is here
+"""
 def add_integer(a, b=98):
+    """
+    Adding 2 integer or float number
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
