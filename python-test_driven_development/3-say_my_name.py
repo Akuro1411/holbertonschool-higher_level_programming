18eb371fb6a413623d9a8632e8ccfaae1b7e0526
Akuro1411 <nahidisayev147@gmail.com>
2024-06-02 03:34:14 +0000

    Module and function were documented

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+Module is documanted
+"""
 def say_my_name(first_name, last_name=""):
+    """
+    Fucntion prints first and last names.
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     elif type(last_name) is not str:
