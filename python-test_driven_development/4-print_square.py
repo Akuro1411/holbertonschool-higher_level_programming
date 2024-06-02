045837fbece637f465d1a9c8607bbd1c3ce17616
Akuro1411 <nahidisayev147@gmail.com>
2024-06-02 18:49:47 +0000

    Pycode

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -2,15 +2,17 @@
 """
 Module is documented
 """
+
+
 def print_square(size):
     """
     Functions is documented
     """
-    if type(size) != int:
+    if type(size) is not int:
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
-    elif type(size) == float and size < 0:
+    elif type(size) is float and size < 0:
         raise TypeError("size must be an integer")
 
     for i in range(size):
