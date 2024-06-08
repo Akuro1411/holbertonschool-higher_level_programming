7e6cb4f41b7be1a5978ba14c9acf69ea5d59aae9
Akuro1411 <nahidisayev147@gmail.com>
2024-06-08 11:07:21 +0000

    Returns the json represantion of object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+Json module for json object
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Returns json represantation of object
+    """
+    return json.dumps(my_obj)
