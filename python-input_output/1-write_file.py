146b1a0f97138c9742fa0159c4dc5672d5f16df5
Akuro1411 <nahidisayev147@gmail.com>
2024-06-08 09:57:51 +0000

    Encoding parametr was deleted

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,7 +8,7 @@ def write_file(filename="", text=""):
     """
     opens the file and writes
     """
-    with open(filename, "w", encode="utf-8") as new_file:
+    with open(filename, "w") as new_file:
         file = new_file.write(text)
     i = len(text)
     return i
