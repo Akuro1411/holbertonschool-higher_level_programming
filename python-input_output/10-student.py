f125574b93051eba3f6201c9e3aee166b3ba6593
Akuro1411 <nahidisayev147@gmail.com>
2024-06-09 08:10:57 +0000

    Students to dict with attr

---
+++ python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+no module
+"""
+
+
+class Student:
+    """
+    Makes student class with name and age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        changes to dict format
+        """
+        student = Student(self.first_name, self.last_name, self.age).__dict__
+        if attrs is not None:
+            new_dict = {}
+            for i in attrs:
+                if i in student:
+                    new_dict[i] = student[i]
+            return new_dict
+        return student
