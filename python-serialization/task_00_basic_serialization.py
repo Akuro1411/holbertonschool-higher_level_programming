ff2d13ecb392e6e9dda0cf7cc25f38664da8ac80
Akuro1411 <nahidisayev147@gmail.com>
2024-06-11 05:49:15 +0000

    Uhh, l must use json for basic serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -2,21 +2,21 @@
 """
 pickle module for serialize and inverse operation
 """
-import pickle
+import json
 
 
 def serialize_and_save_to_file(data, filename):
     """
     serialize data and write it in file
     """
-    with open(filename, "wb") as file:
-        pickle.dump(data, file)
+    with open(filename, "w") as file:
+        json.dump(data, file)
 
 
 def load_and_deserialize(filename):
     """
     deserialize data and read it
     """
-    with open(filename, "rb") as file:
-        data = pickle.load(file)
+    with open(filename, "r") as file:
+        data = json.load(file)
     return data
