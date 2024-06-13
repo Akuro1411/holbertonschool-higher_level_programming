eeb23072617cee9c1ef6ab4805cc13b5bd730070
Akuro1411 <nahidisayev147@gmail.com>
2024-06-13 14:03:51 +0000

    Server set up

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python3
+import http.server
+import json
+import socket
+
+
+class myHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write("Hello, this is a simple API!".encode())
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write("OK".encode())
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write("Undefined endpoint error".encode())
+
+
+port = 8000
+try:
+    server = http.server.HTTPServer(('', port), myHandler)
+    print('Started httpserver on port ', port)
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    print('^C received, shutting down the web server')
+    server.socket.close()
