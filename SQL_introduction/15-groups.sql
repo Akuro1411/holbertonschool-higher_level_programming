e863448d93eee547c33b7921780dc1a4a3a74916
Akuro1411 <nahidisayev147@gmail.com>
2024-06-26 14:16:47 +0000

    Order by didnt work with alias

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Finds the total number of each score. Group by was used on this task
-SELECT score, COUNT(score) "number" FROM second_table GROUP BY score ORDER BY "number" DESC;
+SELECT score, COUNT(score) "number" FROM second_table GROUP BY score ORDER BY COUNT(score) DESC;
