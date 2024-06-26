3049f36c0da835656c8d62bea12bacfef34e6331
Akuro1411 <nahidisayev147@gmail.com>
2024-06-26 14:14:43 +0000

    Order by number

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Finds the total number of each score. Group by was used on this task
-SELECT score, COUNT(score) "number" FROM second_table GROUP BY score;
+SELECT score, COUNT(score) "number" FROM second_table GROUP BY score ORDER BY "number";
