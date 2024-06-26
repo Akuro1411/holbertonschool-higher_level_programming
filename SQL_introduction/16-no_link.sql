48659b067abef2cef4dc03f3859a718ab9482d61
Akuro1411 <nahidisayev147@gmail.com>
2024-06-26 14:24:18 +0000

    Ignore rows without name

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Shows the list of data from table desceding order based on score
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
