bbcdfcebb637e426471e84b34bd35d1aba3b374b
Akuro1411 <nahidisayev147@gmail.com>
2024-06-27 11:05:52 +0000

    Create and grant

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Creates a user on localhost and give all privilages to it
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILAGES ON *.* to 'user_0d_1'@'localhost';
