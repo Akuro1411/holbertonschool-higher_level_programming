54a99fa45d30f6b396834acb8a741446d301bb1e
Akuro1411 <nahidisayev147@gmail.com>
2024-06-27 12:00:57 +0000

    The new table in new database

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Creates table with unique id value.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
+(
+	id INT NOT NULL UNIQUE PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
