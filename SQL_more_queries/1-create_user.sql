bc3c2dc27216ad9dfc772db429c34a780131f250
Akuro1411 <nahidisayev147@gmail.com>
2024-06-27 11:35:48 +0000

    Some errors were corrected

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates a user on localhost and give all privilages to it
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
-GRANT ALL PRIVILAGES ON *.* to 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
