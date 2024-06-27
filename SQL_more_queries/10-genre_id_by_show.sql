0dfb35b5da51954bd26cc80523804cd67cef08e4
Akuro1411 <nahidisayev147@gmail.com>
2024-06-27 13:48:24 +0000

    Comment is added

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,1 +1,2 @@
+-- Joins two table and shows title from one and genre from another one
 SELECT sh.title, gen.genre_id FROM tv_shows sh NATURAL JOIN tv_show_genres gen ORDER BY sh.title, gen.genre_id ASC;
