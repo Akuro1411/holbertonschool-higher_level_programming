9bc5776c2791a54d6b6f0365c51d61b2a649bedc
Akuro1411 <nahidisayev147@gmail.com>
2024-06-27 11:23:55 +0000

    Without quotes

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates a user on localhost and give all privilages to it
-CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILAGES ON *.* to user_0d_1;
+CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILAGES ON *.* to 'user_0d_1';
