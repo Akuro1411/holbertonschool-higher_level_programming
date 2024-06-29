a3325ad0c03830fae4b163865862c9d551258286
Akuro1411 <nahidisayev147@gmail.com>
2024-06-29 20:26:54 +0000

    Id can be different

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 -- Finds the names of genres which Dexter contains
-SELECT gen.name FROM tv_genres gen JOIN tv_show_genres sh ON sh.genre_id = gen.id JOIN tv_shows tv WHERE tv.id = 8 AND tv.id = sh.show_id ORDER BY gen.name ASC;
+SELECT gen.name FROM tv_genres gen JOIN tv_show_genres sh ON sh.genre_id = gen.id JOIN tv_shows tv WHERE tv.title = 'Dexter' AND tv.id = sh.show_id ORDER BY gen.name ASC;
