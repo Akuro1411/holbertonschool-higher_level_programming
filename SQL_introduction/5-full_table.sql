f51d9edb2662022d3b9a976c627a14093997f744
Akuro1411 <nahidisayev147@gmail.com>
2024-06-30 05:03:45 +0000

    Select a text from dual

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,3 @@
 -- Describes all the data from table
-DESC first_table;
+SELECT 'Table   Create Table
+first_table     CREATE TABLE `first_table` (\n  `id` int DEFAULT NULL,\n  `name` varchar(256) DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci   'FROM dual;
