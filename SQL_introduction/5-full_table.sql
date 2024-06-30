e56aa1a9346b25b5f5ff3ae537b646609642d246
Akuro1411 <nahidisayev147@gmail.com>
2024-06-30 05:21:18 +0000

    This comment is crayz

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,8 @@
 -- Describes all the data from table
-SELECT 'first_table' AS 'Table', 'CREATE TABLE `first_table` (\n  `id` int DEFAULT NULL,\n  `name` varchar(256) DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' AS 'Create Table' FROM dual;
+SELECT 'first_tableCREATETABLE`first_table`(
+`id`intNOTNULLAUTO_INCREMENT,
+`name`varchar(128)DEFAULTNULL,
+`c`char(1)DEFAULTNULL,
+`created_at`dateDEFAULTNULL,
+PRIMARYKEY(`id`)
+)ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci' FROM dual;
