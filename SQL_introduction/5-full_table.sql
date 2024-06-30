a747e8f0b824127b5d11e7746477c469e785484b
Akuro1411 <nahidisayev147@gmail.com>
2024-06-30 19:27:56 +0000

    Show table info

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,2 @@
 -- Describes all the data from table
-SELECT 'first_tableCREATETABLE`first_table`(
-`id`intNOTNULLAUTO_INCREMENT,
-`name`varchar(128)DEFAULTNULL,
-`c`char(1)DEFAULTNULL,
-`created_at`dateDEFAULTNULL,
-PRIMARYKEY(`id`)
-)ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci' FROM dual;
+SHOW CREATE TABLE first_table;
