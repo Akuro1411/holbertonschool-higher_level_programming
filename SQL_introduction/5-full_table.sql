1fa16656091e6124cc785216a3748a7ee9074f15
Akuro1411 <nahidisayev147@gmail.com>
2024-06-30 05:17:22 +0000

    Write description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
 -- Describes all the data from table
-SELECT 'Table   Create Table
-first_table     CREATE TABLE `first_table` (\n  `id` int DEFAULT NULL,\n  `name` varchar(256) DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci   'FROM dual;
+SELECT 'first_table' AS 'Table', 'CREATE TABLE `first_table` (\n  `id` int DEFAULT NULL,\n  `name` varchar(256) DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' AS 'Create Table' FROM dual;
