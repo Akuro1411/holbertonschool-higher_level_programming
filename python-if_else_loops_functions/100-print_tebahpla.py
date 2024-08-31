d1459e07ca6218b12131b571a2707997fb3a8a62
Akuro1411 <nahidisayev147@gmail.com>
2024-08-31 09:34:51 +0000

    Format is used for string

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,10 +2,10 @@
 for i in reversed(range(97, 123)):
     if i % 2 == 1:
         i = i - 32
-    print(chr(i), end="")
+    print("{}".format(chr(i)), end="")
 
 # You can get reverse successive object with this way
 # for i in range(122, 96, -1):
 #     if i % 2 == 1:
 #         i = i - 32
-#     prnt(chr(i), end="")
+#     print(chr(i), end="")
