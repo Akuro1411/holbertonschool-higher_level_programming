468cf157e8642e3d7f053d0478ffc35a836f87a0
Akuro1411 <nahidisayev147@gmail.com>
2024-08-31 09:32:44 +0000

    This time l used only one print

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 for i in reversed(range(97, 123)):
     if i % 2 == 1:
-        print(chr(i - 32), end="")
-        continue
+        i = i - 32
     print(chr(i), end="")
 
 # You can get reverse successive object with this way
 # for i in range(122, 96, -1):
-#     print(chr(i), end=" ")
+#     if i % 2 == 1:
+#         i = i - 32
+#     print(chr(i), end="")
