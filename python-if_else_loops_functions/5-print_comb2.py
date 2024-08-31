4ea5d828621a331ecc50cc705d1be49af6416cff
Akuro1411 <nahidisayev147@gmail.com>
2024-08-31 08:34:59 +0000

    New code used the continue

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i == 99:
-        break
-    print(f"{'{:02d}'.format(i)}, ", end="")
-print("99")
+    if i != 99:
+        print("{:02d}".format(i), end=", ")
+        continue
+    print(i)
