6cc2bff70606e917711a6e89dfc59ed04c77676f
Akuro1411 <nahidisayev147@gmail.com>
2024-08-31 09:28:10 +0000

    Remove the element at index

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+def remove_char_at(str, n):
+    new_str = ""
+    for index, i in enumerate(str):
+        if index != n:
+            new_str += i
+    return new_str
+
+# def remove_char_at(str, n):
+#     new_str = ""
+#     for i in range(len(str)):
+#         if i != n:
+#             new_str += str[i]
+#     return new_str
