bf1eff08ef0ea1482d93db556f19519345bbdbbb
Akuro1411 <nahidisayev147@gmail.com>
2024-09-01 16:53:15 +0000

    Empty dict is handled

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     max_key = ""
     max_value = 0
-    if a_dictionary is not None:
+    if a_dictionary is not None and a_dictionary != {}:
         for key, item in a_dictionary.items():
             if item > max_value:
                 max_value = item
