dd0fdfad426233b66d2639e90f55c112bae5a8ef
Akuro1411 <nahidisayev147@gmail.com>
2024-09-01 12:07:48 +0000

    This code is correct

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    """Returns a key with the biggest integer value."""
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
-        return None
-
-    ret = list(a_dictionary.keys())[0]
-    big = a_dictionary[ret]
-    for k, v in a_dictionary.items():
-        if v > big:
-            big = v
-            ret = k
-    return (ret)
+def divisible_by_2(my_list=[]):
+    tf_list = []
+    for i in my_list:
+        if i % 2 == 0:
+            tf_list.append(True)
+            continue
+        tf_list.append(False)
+    return tf_list
