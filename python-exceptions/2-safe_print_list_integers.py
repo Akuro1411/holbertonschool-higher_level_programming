570cb384868865eef86fdae3db8b0764be929b51
Akuro1411 <nahidisayev147@gmail.com>
2024-09-01 19:27:23 +0000

    Except clause is added

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 n += 1
         print()
         return n
-    finally:
+    except (IndexError, TypeError, ValueError):
         print(end="")
+        return n
