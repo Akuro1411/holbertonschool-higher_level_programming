beccb25b2bfe5d9e1fe658684f66b5b7688ea53f
Akuro1411 <nahidisayev147@gmail.com>
2024-09-01 09:07:13 +0000

    The functon should return copy of original list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    elif idx >= len(my_list):
-        return my_list
-    else:
-        new_list = []
-        for i in my_list:
-            new_list.append(i)
-        new_list[idx] = element
-        return new_list
+    copy_list = my_list[:]
+    if idx < 0 or len(copy_list) <= idx:
+        return copy_list
+    copy_list[idx] = element
+    return copy_list
