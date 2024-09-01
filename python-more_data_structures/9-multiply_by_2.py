038d60d2fbf1a5a71d1cfd184a6bd73108c5f6d1
Akuro1411 <nahidisayev147@gmail.com>
2024-09-01 16:42:10 +0000

    Dictionary comprhension is used

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    """Return a new dictionary with all values multipled by 2."""
-    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
+    multiplied_dict = {key: a_dictionary[key] * 2 for key in list(a_dictionary)}
+    return multiplied_dict
