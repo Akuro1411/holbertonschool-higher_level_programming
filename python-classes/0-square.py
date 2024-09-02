adac8b386f2d5cc37580c5dd231d7753909def7b
Akuro1411 <nahidisayev147@gmail.com>
2024-09-02 21:32:13 +0000

    Some strings are added

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Make a class Square."""
 
 
 class Square:
-    """Represent a square."""
+    """ The square class for square."""
     pass
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Make a class Square."""
 
 
 class Square:
-    """Represent a square."""
+    """The square class for square."""
 
     def __init__(self, size=0):
         """Initialize a new Square.
@@ -13,6 +13,6 @@ class Square:
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
