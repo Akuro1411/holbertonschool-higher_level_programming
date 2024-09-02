ac922310da8de437844e073e9006c349f1aa1087
Akuro1411 <nahidisayev147@gmail.com>
2024-09-02 22:14:48 +0000

    Dont fill lines with spaces if [1] > 0

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,59 +1,83 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Make a class Square."""
 
 
 class Square:
-    """Represent a square."""
+    """The square class for square."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new square.
+        """Initialize a new Square.
 
         Args:
             size (int): The size of the new square.
-            position (int, int): The position of the new square.
         """
-        self.size = size
-        self.position = position
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if not isinstance(position, tuple) or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """
+        There is no argument for this method
+        :return: Returns the area of square
+        """
+        area = pow(self.__size, 2)
+        return area
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
-
+        """
+        :return: Returns the size of the square
+        """
+        return self.__size
     @size.setter
     def size(self, value):
+        """
+        :param value: The new size of the square
+        :return: Nothing is returned by function
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     @property
     def position(self):
-        """Get/set the current position of the square."""
-        return (self.__position)
+        """
+        :return: The position of square 
+        """
+        return self.__position
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+        """
+        :param value: 
+        :return: Returns nothing because it is setter 
+        """
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-
-    def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
-
     def my_print(self):
-        """Print the square with the # character."""
-        if self.__size == 0:
+        """
+        :return: No return, prints the square of #
+        """
+        edge = self.__size
+        spaces = self.__position[0]
+        if edge == 0:
             print("")
-            return
-
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+        for i in range(edge):
+            if self.__position[1] == 0:
+                for t in range(spaces):
+                    print("_", end="")
+            for j in range(edge):
+                print("#", end="")
+            print()
