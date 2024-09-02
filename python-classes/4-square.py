445831512f415a2b8a06afe3bedf05fa84d9941b
Akuro1411 <nahidisayev147@gmail.com>
2024-09-02 21:51:52 +0000

    @Property and @size.setter is used for making them behave as getter and setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,31 +1,44 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Make a class Square."""
 
 
 class Square:
-    """Represent a square."""
+    """The square class for square."""
 
     def __init__(self, size=0):
-        """Initialize a new square.
+        """Initialize a new Square.
 
         Args:
             size (int): The size of the new square.
         """
-        self.size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        There is no argument for this method
+        :return: Returns the area of square
+        """
+        area = pow(self.__size, 2)
+        return area
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
-
+        """
+        :return: Returns the size of the square 
+        """
+        return self.__size
     @size.setter
     def size(self, value):
+        """
+        :param value: The new size of the square 
+        :return: Nothing is returned by function
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
