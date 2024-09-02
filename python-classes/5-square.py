78e68954e7f9bcfcd2f23f606118921cc9af91c8
Akuro1411 <nahidisayev147@gmail.com>
2024-09-02 21:56:48 +0000

    Prints the square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,39 +1,54 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Make a class Square."""
 
 
 class Square:
-    """Represent a square."""
+    """The square class for square."""
 
-    def __init__(self, size):
-        """Initialize a new square.
+    def __init__(self, size=0):
+        """Initialize a new Square.
 
         Args:
             size (int): The size of the new square.
         """
-        self.size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        There is no argument for this method
+        :return: Returns the area of square
+        """
+        area = pow(self.__size, 2)
+        return area
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
-
+        """
+        :return: Returns the size of the square
+        """
+        return self.__size
     @size.setter
     def size(self, value):
+        """
+        :param value: The new size of the square
+        :return: Nothing is returned by function
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
-
     def my_print(self):
-        """Print the square with the # character."""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
-        if self.__size == 0:
-            print("")
+        """
+        :return: No return, prints the square of #
+        """
+        edge = self.__size
+        for i in range(edge):
+            for j in range(edge):
+                print("#", end="")
+            print()
