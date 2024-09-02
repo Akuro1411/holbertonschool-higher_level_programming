011911e8f46b362fdf71c463d5e28da1814b997d
Akuro1411 <nahidisayev147@gmail.com>
2024-09-02 17:52:57 +0000

    The new condition for mixed strings

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -20,6 +20,10 @@ def roman_to_int(roman_string):
             sum -= roman_dict[roman_string[i]]
             i = i + 1
     while idx < len(roman_string):
+        if idx != len(roman_string) - 1 and roman_dict[roman_string[idx]] < roman_dict[roman_string[idx + 1]]:
+            sum += (roman_dict[roman_string[idx + 1]] - roman_dict[roman_string[idx]])
+            idx += 2
+            continue
         sum += roman_dict[roman_string[idx]]
         idx += 1
     return sum
