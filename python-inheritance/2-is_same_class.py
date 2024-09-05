37355d1c8feb410bfb3187979fe6cf0239f2ffda
Akuro1411 <nahidisayev147@gmail.com>
2024-09-05 17:16:22 +0000

    Everythin is subclass of object in python

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+"""
+No module is imported.Function is for checking instance
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    :param obj: The object for checking
+    :param a_class: The class for checking
+    :return: Returns True if object belongs to class: otherwise False
+    """
+    return isinstance(obj, a_class)
