44cb9b53a6cc856261da214128f80ce8e23c952e
Akuro1411 <nahidisayev147@gmail.com>
2024-09-05 18:24:58 +0000

    Defines a square from Rectangle class

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+No module is imported.Class for Geometry
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines a square object that parent class is Rectangle"""
+    def __init__(self, size):
+        super().__init__(width=size, height=size)
+        self.__size = size
+
+    def area(self):
+        """
+        :return: The area of square
+        """
+        return self.__size ** 2
