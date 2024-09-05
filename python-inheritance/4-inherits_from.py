8bfd2d69e56f78d09c1fed3969a9a64166e379d3
Akuro1411 <nahidisayev147@gmail.com>
2024-09-05 17:41:24 +0000

    Checks for inherited from class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+"""
+No module is imported.Function is for checking instance
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    :param obj: The object for checking
+    :param a_class: The class for checking
+    :return: Returns True if object belongs to class: otherwise False
+    """
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    return False
