51d70a42bfb5cf5032cdfaf30698ffd646d04322
Akuro1411 <nahidisayev147@gmail.com>
2024-09-05 18:32:44 +0000

    Print the str representation

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+No module is imported.Class for Geometry
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines a square object that parent class is Rectangle"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(width=size, height=size)
+        self.__size = size
+
+    def area(self):
+        """
+        :return: The area of square
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
