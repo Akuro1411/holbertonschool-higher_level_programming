729cc835e416554ec9cfadc157bc2af47c8f273b
Akuro1411 <nahidisayev147@gmail.com>
2024-09-06 12:11:02 +0000

    Bark and meow

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
+"""
+abs module is imported for abstraction
+"""
 from abc import ABC, abstractmethod
 
 
 class Animal(ABC):
-
+    """The base class of all animals"""
     @abstractmethod
     def sound(self):
+        """Abstract method for all derived classes"""
         pass
 
 
 class Dog(Animal):
-
+    """The dog is the subclass of animal"""
     def sound(self):
+        """
+        :return: A dog should bark
+        """
         return "Bark"
 
 
 class Cat(Animal):
-
+    """The cat is the subclass of animal"""
     def sound(self):
+        """
+        :return: A cat should meow
+        """
         return "Meow"
