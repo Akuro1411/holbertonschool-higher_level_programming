294d97242f8a41c2514d150e1f0fb12e7444a37e
Akuro1411 <nahidisayev147@gmail.com>
2024-09-06 16:52:15 +0000

    Extends list class and override functions

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""No module is imported"""
+
+
+class VerboseList(list):
+    """Inherits the class from the list class"""
+    def append(self, __object):
+        """Appends the element to end of the list"""
+        self += [__object]
+        print(f"Added [{__object}] to the list.")
+
+    def extend(self, __iterable):
+        """Appends list of elements to end"""
+        n = len(__iterable)
+        self += __iterable
+        print(f"Extended the list with [{n}] items.")
+
+    def pop(self, __index=-1):
+        """Pops the element for given index(default = -1)"""
+        print(f"Popped [{self[__index]}] from the list.")
+        del self[__index]
+
+    def remove(self, __value):
+        """Removes the element which is equal to value"""
+        for i in range(len(self)):
+            if self[i] == __value:
+                break
+        del self[i]
+        print(f"Removed [{__value}] from the list.")
