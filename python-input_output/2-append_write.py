88652fd6e6f1a1e208b00d2141759790fbf33303
Akuro1411 <nahidisayev147@gmail.com>
2024-09-16 18:35:14 +0000

    There was unnecessary :bug:

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -8,7 +8,6 @@ def append_write(filename="", text=""):
     """
     opens the file and appends the text
     """
-    def append_write(filename="", text=""):
     with open(filename, encoding="UTF-8", mode="a") as file:
         file.write(text)
     return len(text)
