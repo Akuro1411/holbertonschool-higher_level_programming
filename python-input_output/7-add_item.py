bfdee416b70553148dc485fc989eeb2dd238b9d8
Akuro1411 <nahidisayev147@gmail.com>
2024-09-21 10:45:11 +0000

    The brief code

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,40 +1,25 @@
 #!/usr/bin/python3
 """
-json, sys and os were imported for this code
+The modules are used for convert the object and put it in json file
 """
 import json
 from sys import argv
-from os.path import isfile
+import os
 
 
-def save_to_json_file(my_obj, filename):
-    """
-    opens and saves the object in file
-    """
-    with open(filename, "w") as new_file:
-        text = json.dumps(my_obj)
-        new_file.write(text)
-    return new_file
-
-
-def load_from_json_file(filename):
-    """
-    makes python object from json file
-    """
-    with open(filename, "r") as new_file:
-        a = new_file.read()
-    b = json.loads(a)
-    return b
-
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 file = "add_item.json"
-if not isfile(file):
-    arr = []
-else:
-    arr = load_from_json_file(file)
 
-n = len(argv)
-for i in range(1, n):
-    arr.append(argv[i])
+if not os.path.exists("./add_item.json"):
+    storage = []
+    save_to_json_file(storage, file)
 
-save_to_json_file(arr, file)
+storage = load_from_json_file(file)
+for i in range(len(argv)):
+    if i == 0:
+        continue
+    storage.append(argv[i])
+
+save_to_json_file(storage, file)
