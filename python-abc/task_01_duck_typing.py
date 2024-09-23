d7202e4aa03f483cb06431ac0b8a6764e0ba4d3b
Akuro1411 <nahidisayev147@gmail.com>
2024-09-23 17:39:53 +0000

    New code is checked

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,70 +1,48 @@
-#!/usr/bin/env python3
-"""
-abs module is imported for abstraction
-Math module is imported for pi
-"""
+#!/usr/bin/python3
 from abc import ABC, abstractmethod
 from math import pi
 
 
 class Shape(ABC):
-    """Base class for all figures"""
+
     @abstractmethod
     def area(self):
-        """Blueprint method for figures"""
         pass
 
     @abstractmethod
     def perimeter(self):
-        """Blueprint method for figures"""
         pass
 
 
 class Circle(Shape):
-    """The circle class takes radius as an argument"""
+
     def __init__(self, radius):
         self.radius = abs(radius)
 
     def area(self):
-        """
-        :return: Returns the area of the circle
-        """
-        return pi * (self.radius ** 2)
+        return pi * self.radius ** 2
 
     def perimeter(self):
-        """
-        :return: Returns the perimeter of the circle
-        """
-        return 2 * pi * self.radius
+        return pi * self.radius * 2
 
 
 class Rectangle(Shape):
-    """The rectangle class takes width and height of rectangle as arguments"""
+
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
     def area(self):
-        """
-        :return: Returns the area of the rectangle
-        """
         return self.width * self.height
 
     def perimeter(self):
-        """
-        :return: Returns the perimeter of the rectangle
-        """
-        return (self.width + self.height) * 2
+        return 2 * (self.width + self.height)
 
 
 def shape_info(obj):
-    """Prints the area and perimeter of the given object
-       This function implemented for duck typing
-    """
-    # Calculating the area and perimeter
+
     area = obj.area()
     perimeter = obj.perimeter()
 
-    # Printing the area and perimeter
     print(f"Area: {area}")
-    print(f"Perimetr: {perimeter}")
+    print(f"Perimeter: {perimeter}")
