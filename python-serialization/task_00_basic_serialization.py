82c3806f8b66fdb94de03da39f78cd124534dd4e
Akuro1411 <nahidisayev147@gmail.com>
2024-09-29 19:08:06 +0000

    Serialize with proper way

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -9,14 +9,14 @@ def serialize_and_save_to_file(data, filename):
     """
     serialize data and write it in file
     """
-    with open(filename, "w") as file:
-        json.dump(data, file)
+    with open(filename, encoding="UTF-8", mode="w") as file:
+        json.dump(data, fp=file)
 
 
 def load_and_deserialize(filename):
     """
     deserialize data and read it
     """
-    with open(filename, "r") as file:
+    with open(filename, encoding="UTF-8") as file:
         data = json.load(file)
     return data
