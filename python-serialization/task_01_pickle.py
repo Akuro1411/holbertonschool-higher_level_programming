0c4704b867efedaef9c0309cebe321ac4ad7a13e
Akuro1411 <nahidisayev147@gmail.com>
2024-10-03 18:02:29 +0000

    All the functions are documanted

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -6,28 +6,40 @@ import pickle
 
 
 class CustomObject:
+    """The CustomObject class"""
     def __init__(self, name, age, is_student):
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
+        """
+        :return: Displays the attributes of class 
+        """
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
+        """
+        :param filename: 
+        :return: Serializes the data to file
+        """
         try:
             with open(filename, mode="wb") as file:
                 pickle.dump(self, file)
-        except Exception:
+        except IOError:
             return None
 
     @classmethod
     def deserialize(cls, filename):
+        """
+        :param filename: 
+        :return: Deserializes the data from file 
+        """
         try:
             with open(filename, mode="rb") as file:
                 data = pickle.load(file)
             return data
-        except IOError:
+        except Exception:
             return None
