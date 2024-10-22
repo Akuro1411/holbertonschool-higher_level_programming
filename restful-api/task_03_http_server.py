b39041a0ecf7a3e19da8cabc7643440d8de98beb
Akuro1411 <nahidisayev147@gmail.com>
2024-10-22 04:09:28 +0300

    New http server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,45 +1,42 @@
-#!/usr/bin/env python3
-import http.server
+from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 import socket
-
-
-class myHandler(http.server.BaseHTTPRequestHandler):
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
             self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write("Hello, this is a simple API!".encode())
+            self.wfile.write(b"Hello, this is a simple API!")
+
         elif self.path == "/data":
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
+            self.send_header("Content-type", "application/json")
             self.end_headers()
-            data = {"name": "John", "age": 30, "city": "New York"}
-            self.wfile.write(json.dumps(data).encode())
+            data = json.dumps({"name": "John", "age": 30, "city": "New York"})
+            self.wfile.write(data.encode("utf-8"))
+
         elif self.path == "/status":
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
+            self.send_response(200, "OK")
             self.end_headers()
-            self.wfile.write("OK".encode())
+            self.wfile.write(b"OK")
+
         elif self.path == "/info":
             self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
-            data = {"version": "1.0", "description": "A simple API built with http.server"}
-            self.wfile.write(json.dumps(data).encode())
+            data = json.dumps({"version": "1.0", "description": "A simple API built with http.server"})
+            self.wfile.write(data.encode("utf-8"))
+
         else:
             self.send_response(404)
-            self.send_header('Content-type', 'text/plain')
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write("404 Not Found".encode())
+            self.wfile.write(b"Endpoint not found")
 
-port = 8000
 try:
-    server = http.server.HTTPServer(('', port), myHandler)
-    print('Started httpserver on port ', port)
-    server.serve_forever()
-
+    httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
+    httpd.serve_forever()
 except KeyboardInterrupt:
-    print('^C received, shutting down the web server')
-    server.socket.close()
+    print("The server was stopped")
+    httpd.socket.close()
