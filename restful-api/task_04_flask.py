4e65f3420386e91c28919d7b101c3e8c21530009
Akuro1411 <nahidisayev147@gmail.com>
2024-10-22 21:18:58 +0300

    New flask app

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,39 +1,45 @@
-#!/usr/bin/env python3
 import flask
-from flask import Flask, jsonify, request
+from flask import jsonify
+from flask import request
+app = flask.Flask(__name__)
 
-app = Flask(__name__)
+users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"},
+         "arnold": {"name": "Arnold", "age": 30, "city": "Los Angeles"}
+         }
 
-users = {}
+
 @app.route('/')
 def home():
-    return 'Welcome to the Flask API!'
+    return "Welcome to the Flask API!"
+
 
 @app.route('/data')
-def data():
-    arr = []
-    for i in users:
-        arr.append(i)
-    return jsonify(arr)
+def user_list():
+    return jsonify(users)
+
 
 @app.route('/status')
 def status():
-    return 'OK'
+    return "OK"
 
-@app.route('/users/<username>')
-def get_user(username):
-    if users[username]:
-        return jsonify(users[username])
-    else:
-        return jsonify({"error": "User not found"})
-@app.route("/add_user", methods=["POST"])
+
+@app.route('/user/<username>')
+def get_user_info(username):
+    if username not in users:
+        return {"error": "User not found"}
+    return jsonify(users[username])
+
+
+@app.route('/add_user', methods=["POST"])
 def add_user():
-    new_user = request.get_json()
-    user = new_user.get("username")
+    data = request.get_json()
+    user = data.get("username")
     if not user:
-        return jsonify({"error": "Username is required"}), 400
-    users[user] = new_user
-    return jsonify({"message": "User added", "user": new_user}), 201
+        return jsonify({"error": "Username is required"}), 404
+    users[user] = data
+    response_post = {"message": "User added", "user": data}
+    return jsonify(response_post), 201
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000)
+
