1d628d9790dd1be910fb54b2f2f0beec43f63567
Akuro1411 <nahidisayev147@gmail.com>
2024-11-12 22:20:59 +0300

    Code is protected from sql injection

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -9,14 +9,12 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=argv[1], password=argv[2], database=argv[3])
     cursor = db.cursor()
     query = "select c.id, c.name, s.name from cities c \
-    join states s where c.state_id=s.id and binary s.name=%s order by c.id"
+    join states s on c.state_id=s.id order by c.id"
 
-    cursor.execute(query, (argv[4], ))
+    cursor.execute(query)
     rows = cursor.fetchall()
-    for index, row in enumerate(rows):
-        if index != len(rows) - 1:
-            print(row[1], end=", ")
-        else:
-            print(row[1])
+    cities = [row[1] for row in rows if row[2] == argv[4]]
+    print(", ".join(cities))
+    
     cursor.close()
     db.close()
