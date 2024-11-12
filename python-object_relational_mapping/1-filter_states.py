ee1287940e5b1c13d5ba91802b4d16f398399d38
Akuro1411 <nahidisayev147@gmail.com>
2024-11-12 19:37:08 +0300

    Pycodestyle error is solved

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=argv[1], password=argv[2], database=argv[3])
     cursor = db.cursor()
 
-    cursor.execute("Select * from states where name like %s order by id", ('N%',))
+    cursor.execute("Select * from states where name like 'N%' order by id")
     rows = cursor.fetchall()
     for row in rows:
         # it could be written with conditional statement too
