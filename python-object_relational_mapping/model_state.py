b0613a223780ff2bc8680e26c2f345943f7f6276
Akuro1411 <nahidisayev147@gmail.com>
2024-11-20 17:51:49 +0300

    The file is documented

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Modules for sqlalchemy"""
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import declarative_base
 
@@ -7,6 +8,8 @@ Base = declarative_base()
 
 
 class State(Base):
+    """State class"""
     __tablename__ = 'states'
     id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+
