79362e2761e1a20c47fe8fd42564e3d84f4e1eaf
Akuro1411 <nahidisayev147@gmail.com>
2024-11-21 00:18:16 +0300

    File is documented and pycodestyle

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -3,7 +3,9 @@
 from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
 from model_state import Base
 
+
 class City(Base):
+    """City mapped class"""
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
