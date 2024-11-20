c1d7af912e4e7ee866309f0d8a353c3cc3e78a9d
Akuro1411 <nahidisayev147@gmail.com>
2024-11-20 17:47:19 +0300

    Model states

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""Module containing State class"""
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base
 
+engine = create_engine('mysql:///:memory:', echo=True)
 Base = declarative_base()
 
 
 class State(Base):
-    """The State class"""
     __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True, unique=True)
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
