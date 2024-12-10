24f33a526140e6df37c3af6f0e3818a3c81befef
Akuro1411 <nahidisayev147@gmail.com>
2024-12-10 06:27:56 +0300

    Code with error handlings

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -2,17 +2,26 @@
 """There is no module here"""
 
 
-def generate_invitations(template, attendees):
+def generate_invitations(default_template, attendees):
     """Generates different templates from default one"""
-    default_template = template
-    for obj in attendees:
-        template = default_template
-        for key in obj:
-            if obj[key] is None:
-                template = template.replace(f"{{{key}}}", "N/A")
-                continue
-            template = template.replace(f"{{{key}}}", obj[key])
-        idx = attendees.index(obj)
-        new = template
-        with open(f"output_{idx}.txt", "w") as f:
-            f.write(new)
+    try:
+        isinstance(default_template, str) or isinstance(attendees, list)
+        try:
+            for obj in attendees:
+                try:
+                    template = default_template
+                except Exception:
+                    return "Template is empty, no output files generated."
+                for key in obj:
+                    if obj[key] is None:
+                        template = template.replace(f"{{{key}}}", "N/A")
+                        continue
+                    template = template.replace(f"{{{key}}}", obj[key])
+                idx = attendees.index(obj)
+                new = template
+                with open(f"output_{idx}.txt", "w") as f:
+                    f.write(new)
+        except IndexError:
+            return "No data provided, no output files generated."
+    except Exception:
+        return "Invalid input"
