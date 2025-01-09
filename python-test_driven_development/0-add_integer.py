c423ffa6dc521425ed9e5fec3404bf914c333ee6
Akuro1411 <nahidisayev147@gmail.com>
2025-01-09 21:43:46 +0300

    The function and module is documented

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
+import doctest
+"""Doctest module is imported for testing"""
+
+
 def add_integer(a, b=98):
+    """Function adds 2 number and finds their final value"""
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     elif type(b) not in [int, float]:
         raise TypeError("b must be an integer")
 
     return round(a) + round(b)
+
+
+doctest.testfile("data.txt")
