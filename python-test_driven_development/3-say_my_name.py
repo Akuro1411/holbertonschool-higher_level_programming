f6011910cd25adc7beb0d73f8979cc8744a5f4e4
Akuro1411 <nahidisayev147@gmail.com>
2025-01-12 10:04:55 +0300

    Raised errors are corrected

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,8 +4,10 @@
 
 def say_my_name(first_name, last_name=""):
     """Function prints the first_name and last_name"""
-    if type(first_name) not in [str] or type(last_name) not in [str]:
-        raise TypeError("first_name must be a string or \
-last_name must be a string")
+    if type(first_name) not in [str]:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) not in [str]:
+        raise TypeError("last_name must be a string")
 
     print(f"My name is {first_name} {last_name}")
