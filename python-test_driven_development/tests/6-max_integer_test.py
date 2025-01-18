d9e6c166ef51a75b0e4e82938146b95a141075af
Akuro1411 <nahidisayev147@gmail.com>
2025-01-18 15:11:40 +0300

    Max middle, end and type

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,11 +9,17 @@ class TestMaxInteger(unittest.TestCase):
     def test_0_empty(self):
         self.assertIsNone(max_integer())
 
-    def test_1_nonempty(self):
+    def test_1_max_end(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-    def test_2_type(self):
-        arr = [1, 2, 3, 4]
+    def test_2_max_start(self):
+        self.assertEqual(max_integer([5, 4, 3, 2]), 5)
+
+    def test_3_max_middle(self):
+        self.assertEqual(max_integer([1, 2, 7, 4]), 7)
+
+    def test_4_type(self):
+        arr = [1, 2, 3, 4, "Hello"]
         for element in arr:
             self.assertEqual(type(element), int)
 
